55d548a8395308128f95e4fdbb04da5a08253ea9
Caio de Souza Lima <caio.lima@wises.com.br>
2022-03-05 18:37:50 -0300

    Alteracoes

--- com/souza/caio/fundamentosJava8/encapsulamento/casaB/Maria.java
+++ com/souza/caio/fundamentosJava8/encapsulamento/casaB/Maria.java
@@ -1,5 +1,16 @@
 package com.souza.caio.fundamentosJava8.encapsulamento.casaB;
 
+import com.souza.caio.fundamentosJava8.encapsulamento.casaA.Ana;
+
 public class Maria {
 
+	void testAcessos() {
+		Ana sogra = new Ana();
+		Pedro marido = new Pedro();
+		//System.out.println(sogra.segredo); Não pode pois é private
+		//System.out.println(sogra.fazDentroDeCasa); Não pode pois é public somente no package
+		//System.out.println(sogra.formaDeFalar); Não pode pois é public somente no package e passado por herança
+		System.out.println(sogra.todosSabem);
+		//System.out.println(marido.formaDeFalar); Não pode pois somente pode no pacote da classe Ana
+	}
 }
